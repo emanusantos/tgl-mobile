9b22347831c1fc700fb016874e34ce5b09cffbbe
Emanuel Lucas Mizael dos Santos <79867442+mizaelucas@users.noreply.github.com>
2021-09-05 21:33:00 -0300

    added open and close drawer blur logic

--- screens/NewBet.tsx
+++ screens/NewBet.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { createDrawerNavigator, DrawerScreenProps } from '@react-navigation/drawer';
+import { createDrawerNavigator, DrawerScreenProps, useDrawerStatus } from '@react-navigation/drawer';
 import Cart from '../components/Cart';
 
 type RootStackParamList = {
@@ -12,20 +12,35 @@ type RootStackParamList = {
 const cartDrawer = createDrawerNavigator();
 
 function NewBet({ navigation }: DrawerScreenProps<RootStackParamList>) {
+    const drawer = useDrawerStatus();
+    const [opacity, setOpacity] = useState(1);
+
+    useEffect(() => {
+        if (drawer === 'open') {
+            return setOpacity(.2);
+        };
+
+        if (drawer === 'closed') {
+            return setOpacity(1);
+        };
+    }, [navigation, drawer]);
+
     return (
-        <View style={styles.container}>
-            <Text style={{ fontSize: 22, fontStyle: 'italic', fontWeight: 'bold', color: '#707070' }}>NEW BET FOR</Text>
-            <Text style={{ fontSize: 17, fontStyle: 'italic', color: '#868686', paddingVertical: 15 }}>Choose a game</Text>
-            <TouchableOpacity onPress={() => {navigation.openDrawer()}}>
-                <Text>Drawer!</Text>
-            </TouchableOpacity>
+        <View style={{...styles.container, opacity: opacity}}>
+            <View>
+                <Text style={{ fontSize: 22, fontStyle: 'italic', fontWeight: 'bold', color: '#707070' }}>NEW BET FOR</Text>
+                <Text style={{ fontSize: 17, fontStyle: 'italic', color: '#868686', paddingVertical: 15 }}>Choose a game</Text>
+                <TouchableOpacity onPress={() => navigation.openDrawer()}>
+                    <Text>Drawer!</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 };
 
 const NewBetCart = () => (
     <cartDrawer.Navigator drawerContent={Cart} screenOptions={{ drawerStyle: { width: '60%', borderRadius: 20 }, drawerPosition: 'right', swipeEnabled: false }}>
-        <cartDrawer.Screen name='Cart' component={NewBet} options={{ headerShown: false, sceneContainerStyle: { backgroundColor: '#f5f5f5', opacity: 1 }, overlayColor: 'rgba(0, 0, 0, 0.02)'} } />
+        <cartDrawer.Screen name='Cart' component={NewBet} options={{ headerShown: false, sceneContainerStyle: { backgroundColor: '#fff', opacity: 1 }, overlayColor: 'rgba(0, 0, 0, 0.02)'} } />
     </cartDrawer.Navigator>
 );
 
