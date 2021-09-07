a79a1ac01f8e2809653818a2ebb2bd72ca6d9736
Emanuel Lucas Mizael dos Santos <79867442+mizaelucas@users.noreply.github.com>
2021-09-07 12:37:29 -0300

    added buttons test component

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 export default function Home() {
     return (
         <View style={styles.container}>
             <Text style={{ fontSize: 22, fontStyle: 'italic', fontWeight: 'bold', color: '#707070' }}>RECENT GAMES</Text>
             <Text style={{ fontSize: 17, fontStyle: 'italic', color: '#868686', paddingVertical: 15 }}>Filters</Text>
+            <View style={{ flexDirection: 'row' }}>
+                <TouchableOpacity>
+                    <View style={{ width: 100, borderWidth: 2, borderColor: '#7F3992', paddingVertical: 5, borderRadius: 100, marginRight: 10, alignItems: 'center' }}>
+                        <Text style={{ fontSize: 14, fontStyle: 'italic', fontWeight: 'bold', color: '#7F3992' }}>Lotofácil</Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <View style={{ width: 100, borderWidth: 2, borderColor: '#01AC66', paddingVertical: 5, borderRadius: 100, marginRight: 10, alignItems: 'center' }}>
+                        <Text style={{ fontSize: 14, fontStyle: 'italic', fontWeight: 'bold', color: '#01AC66' }}>Mega-Sena</Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <View style={{ width: 100, borderWidth: 2, borderColor: '#F79C31', paddingVertical: 5, borderRadius: 100, marginRight: 10, alignItems: 'center' }}>
+                        <Text style={{ fontSize: 14, fontStyle: 'italic', fontWeight: 'bold', color: '#F79C31' }}>Quina</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 };
